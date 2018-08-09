0151de2eccbe07baada764e0b6cb30c538101481
Brian Zanti <brianzanti@gmail.com>
2018-08-09 11:15:38 -0600

    updates summary format

--- lib/games.rb
+++ lib/games.rb
@@ -1,7 +1,9 @@
 class Games
-  attr_reader :events
+  attr_reader :events,
+              :year
 
-  def initialize
+  def initialize(year)
+    @year = year
     @events = []
   end
 
@@ -9,25 +11,29 @@ class Games
     @events << event
   end
 
-  def print_summary
-    print_headers
-    print_events
+  def summary
+    headers + "\n" + all_events_summary
   end
 
-  def print_headers
-    puts "Event".ljust(15) +
-         "Max Age of Participant".ljust(20) +
-         "Average Age of Participant".ljust(20) +
-         "Standard Deviation of Ages".ljust(20)
+  def headers
+    "Event".ljust(15) +
+    "Max Age".ljust(20) +
+    "Min Age".ljust(20) +
+    "Average Age".ljust(20) +
+    "StdDev Age"
   end
 
-  def print_events
-    @events.each do |event|
-      puts event.name.ljust(15) +
-           event.max_age.ljust(20) +
-           event.min_age.ljust(20) +
-           event.average_age.ljust(20) +
-           event.standard_deviation_age.ljust(20)
-    end
+  def event_summary(event)
+    event.name.ljust(15) +
+    event.max_age.to_s.ljust(20) +
+    event.min_age.to_s.ljust(20) +
+    event.average_age.to_s.ljust(20) +
+    event.standard_deviation_age.to_s
+  end
+
+  def all_events_summary
+    @events.map do |event|
+      event_summary(event)
+    end.join("\n")
   end
 end
